c3118cad9d6db5f03ea11fc59411aa29816c52c5
ETAvadh <developer.empiere@gmail.com>
2025-02-27 16:44:39 +0530

    - Screen Open Street Map: Implemented open street map get route api. - Screen Open Street Map: Added textfield to search the destination location. - Screen Open Street Map: Displayed the Polyline from the current location to destination. - pubspec: Added and registered the necessary plugin.

--- lib/screen_open_street_map.dart
+++ lib/screen_open_street_map.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
@@ -40,29 +41,91 @@ class _ScreenOpenStreetMapState extends State<ScreenOpenStreetMap> {
       ),
       body: Stack(
         children: [
-          FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              initialCenter: currentLocation ?? const LatLng(22.302567, 70.796376), // can be kept LatLng(0, 0)
-              initialZoom: 15,
-              minZoom: 2,
-              maxZoom: 20,
-            ),
-            children: [
-              TileLayer(urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"),
-              CurrentLocationLayer(
-                style: const LocationMarkerStyle(
-                  marker: DefaultLocationMarker(
-                    child: Icon(
-                      Icons.location_pin,
-                      color: Colors.white,
+          isLoading == true
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : FlutterMap(
+                  mapController: mapController,
+                  options: MapOptions(
+                    initialCenter: currentLocation ?? const LatLng(22.302567, 70.796376), // can be kept LatLng(0, 0)
+                    initialZoom: 15,
+                    minZoom: 2,
+                    maxZoom: 20,
+                  ),
+                  children: [
+                    TileLayer(urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"),
+                    CurrentLocationLayer(
+                      style: const LocationMarkerStyle(
+                        marker: DefaultLocationMarker(
+                          child: Icon(
+                            Icons.location_pin,
+                            color: Colors.white,
+                          ),
+                        ),
+                        markerSize: Size(35, 35),
+                        markerDirection: MarkerDirection.top,
+                      ),
+                    ),
+                    if (destination != null)
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            point: destination!,
+                            height: 52,
+                            width: 50,
+                            child: const Icon(Icons.location_pin, color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    // if (currentLocation != null && destination != null && route.isNotEmpty)
+                    PolylineLayer(
+                      polylines: [
+                        Polyline(
+                          points: route,
+                          strokeWidth: 5,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: locationController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Enter your location',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                      ),
                     ),
                   ),
-                  markerSize: Size(35, 35),
-                  markerDirection: MarkerDirection.top,
-                ),
+                  IconButton(
+                    onPressed: () {
+                      final location = locationController.text.trim();
+                      if (location.isNotEmpty) {
+                        getCordinates(location);
+                      }
+                    },
+                    style: IconButton.styleFrom(backgroundColor: Colors.white),
+                    icon: const Icon(Icons.search),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
@@ -90,6 +153,15 @@ class _ScreenOpenStreetMapState extends State<ScreenOpenStreetMap> {
   Future<void> initializeLocation() async {
     if (!await checkRequestPermission()) {
       return;
+    }
+
+    // Fetch initial location before subscribing
+    LocationData? initialLocation = await location.getLocation();
+    if (initialLocation.latitude != null && initialLocation.longitude != null) {
+      setState(() {
+        currentLocation = LatLng(initialLocation.latitude!, initialLocation.longitude!);
+        isLoading = false;
+      });
     }
 
     location.onLocationChanged.listen((LocationData locData) {
@@ -140,7 +212,7 @@ class _ScreenOpenStreetMapState extends State<ScreenOpenStreetMap> {
           setState(() {
             destination = LatLng(lat, lon);
           });
-          // await fetchRoute();
+          await fetchRoute();
           // method to fetch the route between current location and the destination using OSRM api.
         } else {
           errorMessage('Location not found, please try another search.');
@@ -152,6 +224,44 @@ class _ScreenOpenStreetMapState extends State<ScreenOpenStreetMap> {
       log('$tag error: ${error.toString()}');
       return;
     }
+  }
+
+  Future<void> fetchRoute() async {
+    if (currentLocation == null || destination == null) {
+      return;
+    }
+
+    const String tag = 'get_osm_route';
+
+    final url = Uri.parse("http://router.project-osrm.org/route/v1/driving/"
+        '${currentLocation!.longitude},${currentLocation!.latitude};'
+        '${destination!.longitude},${destination!.latitude}?overview=full&geometries=polyline');
+    log('$tag URL: GET: $url');
+
+    try {
+      final response = await http.get(url);
+      log('$tag response: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final geometry = data['routes'][0]['geometry'];
+        decodePolyline(geometry);
+      } else {
+        errorMessage('Failed to fetch route, Try again later.');
+      }
+    } catch (error) {
+      log('$tag error: ${error.toString()}');
+      return;
+    }
+  }
+
+  void decodePolyline(String encodedPolyline) {
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<PointLatLng> decodedPoints = polylinePoints.decodePolyline(encodedPolyline);
+
+    setState(() {
+      route = decodedPoints.map((point) => LatLng(point.latitude, point.longitude)).toList();
+    });
   }
 
   void errorMessage(String message) {
